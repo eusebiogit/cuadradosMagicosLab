c4a92bc6da03c9f7b5a0a531b7b999a56ce8e438
ordenador <eusebio.guijarro@protonmail.com>
2019-05-17 14:12:46 +0200

    iniciar

---
+++ src/cuadradosmagicoslab/CuadradosMagicosLab.java
@@ -0,0 +1,17 @@
+package cuadradosmagicoslab;
+
+/**
+ *
+ * @author ordenador
+ */
+public class CuadradosMagicosLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
